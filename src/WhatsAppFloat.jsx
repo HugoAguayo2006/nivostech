74420d37bef3a93749d6c60fbbba1a776ab52247
HugoAguayo2006 <hugoaguayo50@gmail.com>
2026-01-14 17:15:29 -0600

    NIVOSTECH

--- src/WhatsAppFloat.jsx
+++ src/WhatsAppFloat.jsx
@@ -1,5 +1,5 @@
 export default function WhatsAppFloat() {
-  const url = "https://wa.me/523310392675?text=Hola,%20me%20gustaría%20saber%20más%20sobre%20NIVO%20y%20sus%20páginas%20web.";
+  const url = "https://wa.me/523310392675?text=Hola,%20me%20gustaría%20saber%20más%20sobre%20NIVOSTECH%20y%20sus%20páginas%20web.";
 
   return (
     <a
